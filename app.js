80b67e057e3dc09c8eaadd32618c8e5c5f4ae9e0
michaelberish <mikeyberish@gmail.com>
2022-05-04 13:15:06 -0700

    done

--- app.js
+++ app.js
@@ -3,15 +3,28 @@
 //type npm install express
 
 //create a variable called express and require 'express'
-
+const express = require('express');
 //create a variable called app and set it equal to express()
-
+const app = express();
 //create a variable called port and set it equal to 3000
-
+const {engine} = require('express-handlebars');
+const port = 3000;
+app.engine('handlebars', engine());
+app.set('view egine', 'handlebars');
+app.set('views', './views');
+app.use('/css', express.static(__dirname + '/views/css'));
 //create a get route (app.get) and use res.send to send text
+app.get('/', (req, res) => {
+    res.render('home', {pageTitle: 'Home Page'})
+})
 
+app.get('/contact', (req,res) => {
+    res.send('Contact');
+})
 //make sure that your app is listening on the port
-
+app.listen(port,() => {
+    console.log(`Listening on Port: ${port}`);
+})
 //console.log `app listening on ${port}
 
 //run the app from the terminal (node app.js)
